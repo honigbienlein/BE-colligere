78ec20fd3c446c7ffd547ea3f76c84a6a70d2ab3
Nettester <github@gluch.eu>
2022-08-30 11:57:42 +0200

    add router for books route

---
+++ routes/booksRoute.js
@@ -0,0 +1,10 @@
+
+import express from "express"
+import {bookByISBN} from "../controller/bookByISBNController.js"
+
+const router = express.Router()
+
+router.route('/:isbn')
+    .get(bookByISBN)
+
+export default router
