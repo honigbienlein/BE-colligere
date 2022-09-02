85ad4835db5d509c227df06a091f94dc987fb26c
Nettester <github@gluch.eu>
2022-09-02 20:32:54 +0200

    use User.sync() / set new error message for unique username

--- controller/user/userPostController.js
+++ controller/user/userPostController.js
@@ -15,6 +15,7 @@ const user_post = async (request, response) => {
     let userToAdd
     const usernameCharLength = 3
     const usernameCharLengthFailedText = `Your username is shorter than ${usernameCharLength} chars.`
+    const errorMessageUniqueUsernameFailed = "Your username is already used."
     const emailLengthFailedText = "Your email is missing."
     const errorMessageUniqueEmailFailed = "Your email is already used."
     const passwordLengthFailedText = "Your password is missing."
@@ -27,6 +28,9 @@ const user_post = async (request, response) => {
     }
 
     try {
+        // if table not exist sync creates a table
+        await User.sync()
+        // add new user to db
         const userInDB = await User.create(userToAdd)
         delete userInDB.dataValues.password
         await response.send(userInDB)
@@ -35,11 +39,12 @@ const user_post = async (request, response) => {
         const emailFailed = !userToAdd.email ? emailLengthFailedText : emptyString
         const passwordFailed = !userToAdd.password ? passwordLengthFailedText : emptyString
         const uniqueEmailFailed = errors[0].message==="email must be unique" ? errorMessageUniqueEmailFailed : emptyString
+        const uniqueUsernameFailed = errors[0].message==="username must be unique" ? errorMessageUniqueUsernameFailed : emptyString
         
-        if(usernameFailed||emailFailed||passwordFailed||uniqueEmailFailed){
+        if(usernameFailed||uniqueUsernameFailed||emailFailed||passwordFailed||uniqueEmailFailed){
             delete userToAdd.password
             await response.send({
-                errorMessages:[usernameFailed, emailFailed, passwordFailed, errorMessageUniqueEmailFailed],
+                errorMessages:[usernameFailed, uniqueUsernameFailed, emailFailed, passwordFailed, errorMessageUniqueEmailFailed],
                 data:{...userToAdd}
             })
         } else {
