fae20e641198c7ddab7c52385c6417a0dc816e41
Nettester <github@gluch.eu>
2022-09-03 14:09:10 +0200

    change response to one send function

--- controller/collection/collectionPostController.js
+++ controller/collection/collectionPostController.js
@@ -7,9 +7,11 @@ import Collection from '../../models/collectionModel.js'
  */
 const users_id_collections_post = async (request, response) => {
 	const userId = request.params.id_user
-	const collectionNameCharLength = 31
-	const collectionNameCharLengthFailedText = `Your name for the collection is longer than ${collectionNameCharLength} chars.`
+	const collectionNameCharLength = 30
+	const collectionNameCharLengthFailedText = `Your name for the collection is shorter then 3 or longer than ${collectionNameCharLength} chars.`
 	const emptyString = ''
+
+	let toSend
 
 	const collectionToAdd = {
 		id_user: userId,
@@ -18,23 +20,27 @@ const users_id_collections_post = async (request, response) => {
 	}
 
 	try {
-		const collectionInDB = await Collection.create(collectionToAdd)
-		await response.send(collectionInDB)
+		await Collection.sync()
+		toSend = await Collection.create(collectionToAdd)
 	} catch ({ errors }) {
+		const errorMessage = Object.values(errors)
+		console.log(errorMessage)
 		const collectionNameFailed =
 			!collectionToAdd.name_collection || collectionToAdd.name_collection > collectionNameCharLength
 				? collectionNameCharLengthFailedText
 				: emptyString
 
 		if (collectionNameFailed) {
-			await response.send({
+			toSend = {
 				errorMessages: [collectionNameFailed],
 				data: { ...collectionToAdd },
-			})
+			}
 		} else {
-			response.send(errors)
+			toSend = { ...errors }
 		}
 	}
+
+	response.send(toSend)
 }
 
 export { users_id_collections_post }
