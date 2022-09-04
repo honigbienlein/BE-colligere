ede23dfe2a30281de1e765ea42bb9a5a760651d0
Nettester <github@gluch.eu>
2022-09-04 18:24:35 +0200

    add attributeTypeModel

--- models/attributeTypeModel.js
+++ models/attributeTypeModel.js
@@ -12,28 +12,9 @@ const AttributeType = db.define(
 			primaryKey: true,
 			allowNull: false,
 		},
-		username: {
+		type: {
 			type: DataTypes.STRING(30),
 			allowNull: false,
-		},
-		email: {
-			type: DataTypes.CITEXT,
-			unique: true,
-			allowNull: false,
-		},
-		password: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		visible: {
-			type: DataTypes.BOOLEAN,
-			allowNull: false,
-			defaultValue: true,
-		},
-		verified_email: {
-			type: DataTypes.BOOLEAN,
-			allowNull: false,
-			defaultValue: true,
 		},
 	},
 	{
@@ -43,4 +24,4 @@ const AttributeType = db.define(
 	},
 )
 
-export default User
+export default AttributeType
