e90aa830c0869123c801a2a89dbc841760a4749e
Nettester <github@gluch.eu>
2022-09-05 09:57:55 +0200

    get collections of a user

--- controller/collection/collectionGetController.js
+++ controller/collection/collectionGetController.js
@@ -1,5 +1,21 @@
-const users_id_collections = (request, response) => {
-	response.send('users_id_collections')
+import Collection from '../../models/collectionModel'
+
+const users_id_collections = async (request, response) => {
+	const id_user = request.params.id_user
+	let dataResponse
+	try {
+		const userCollections = await Collection.findAll({
+			where: { id_user: id_user },
+			attributes: ['id_collection', 'id_user', 'id_template', 'name_collection'],
+		})
+
+		dataResponse = userCollections ?? 'No collections!'
+	} catch (error) {
+		console.log(error)
+		dataResponse = "Something went wrong, can't get a user."
+	}
+
+	response.send(dataResponse)
 }
 const users_id_collections_id = (request, response) => {
 	response.send('users_id_collections_id')
